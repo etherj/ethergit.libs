62957f8d279c3a4b00299e7b1885874475333505
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2015-07-16 18:37:19 +0500

    Fix a problem with bootstrap loading

--- libs.js
+++ libs.js
@@ -12,19 +12,28 @@ define(function(require) {
         
         var plugin = new Plugin('Ethergit', main.consumes);
 
-        var jquery = (require('./jquery.min'), window.jQuery); // Leave global jquery
         var lodash = (require('./lodash.min'), window._);
 
-        // bootstrap depends on jquery, but mini_require doesn't allow to define dependency.
-        setTimeout(function() { require('./bootstrap/js/bootstrap'); }, 1000);
-        
-        plugin.freezePublicAPI({
-            jquery: function() { return jquery; },
-            lodash: function() { return lodash; }
-        });
-        
-        register(null, {
-            'ethergit.libs': plugin
+        async.series({
+            jquery: function(cb) {
+                require(['./jquery.min'], function() {
+                    cb(null, window.jQuery);
+                });
+            },
+            bootstrap: function(cb) {
+                require(['./bootstrap/js/bootstrap'], function() {
+                    cb();
+                });
+            }
+        }, function(err, libs) {
+            plugin.freezePublicAPI({
+                jquery: function() { return libs.jquery; },
+                lodash: function() { return lodash; }
+            });
+
+            register(null, {
+                'ethergit.libs': plugin
+            });
         });
     }
 });
